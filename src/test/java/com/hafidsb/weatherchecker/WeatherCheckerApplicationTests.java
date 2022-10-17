12043c45bdc8eab7c05e533c8dc86241c13024fa
Hafid Sriwijaya <32842793+hafidsb@users.noreply.github.com>
2022-10-17 09:21:50 +0700

    add Spring basic files

---
+++ src/test/java/com/hafidsb/weatherchecker/WeatherCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hafidsb.weatherchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
