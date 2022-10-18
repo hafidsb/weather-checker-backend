afbf3f46981d744f3b9b2ea7d2bd70256ba53ef6
Hafid Sriwijaya <32842793+hafidsb@users.noreply.github.com>
2022-10-18 15:20:26 +0700

    change return type of weather endpoints

--- src/main/java/com/hafidsb/weatherchecker/controller/WeatherController.java
+++ src/main/java/com/hafidsb/weatherchecker/controller/WeatherController.java
@@ -1,9 +1,14 @@
 package com.hafidsb.weatherchecker.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hafidsb.weatherchecker.service.WeatherService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/weather")
@@ -13,17 +18,33 @@ public class WeatherController {
     private final WeatherService weatherService;
 
     @GetMapping
-    public String getWeather(
+    public JsonNode getWeather(
             @RequestParam String lat,
             @RequestParam String lon
     ) {
-        return weatherService.getWeather(lat, lon);
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = weatherService.getWeather(lat, lon);
+
+        try {
+            return mapper.readTree(jsonString);
+        } catch (JsonProcessingException ex) {
+            log.error(ex.getMessage());
+            return null;
+        }
     }
 
     @GetMapping("/city/{id}")
-    public String getWeatherByCityId(
+    public JsonNode getWeatherByCityId(
             @PathVariable Long id
     ) {
-        return weatherService.getWeatherByCityId(id);
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = weatherService.getWeatherByCityId(id);
+
+        try {
+            return mapper.readTree(jsonString);
+        } catch (JsonProcessingException ex) {
+            log.error(ex.getMessage());
+            return null;
+        }
     }
 }
